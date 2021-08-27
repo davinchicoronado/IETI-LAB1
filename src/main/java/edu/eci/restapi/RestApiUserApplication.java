f953f9d1ed19fc31016ee182c0dd8bc9a56a5455
David Coronado <david.coronado@escuelaing.edu.co>
2021-08-27 16:27:45 -0500

    create project java with spring initializr

---
+++ src/main/java/edu/eci/restapi/RestApiUserApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/restapi/RestApiUserApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
